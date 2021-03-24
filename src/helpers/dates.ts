449aa508a105bd85434b3ca1ba40143455457838
Gustavo Czobel <czobel@ubuntuW10>
2021-03-24 20:43:53 +0200

    fix: as master

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -12,22 +12,3 @@ export default function getAllMonthMoments(startMoment: Moment | string, endMome
 
   return allMonths;
 }
-
-export function getAllMonthMoments2(startMoment: Moment | string, includeNext: boolean) {
-  let monthMoment = moment(startMoment).startOf('month');
-
-  const allMonths: Moment[] = [];
-  let lastMonth = moment().startOf('month');
-  if (includeNext) {
-    lastMonth = lastMonth.add(1, 'month');
-    lastMonth = lastMonth.add(1, 'month');
-    lastMonth = lastMonth.add(1, 'month');
-    lastMonth = lastMonth.add(1, 'month');
-  }
-  while (monthMoment.isSameOrBefore(lastMonth)) {
-    allMonths.push(monthMoment);
-    monthMoment = moment(monthMoment).add(1, 'month');
-  }
-
-  return allMonths;
-}
