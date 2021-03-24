2965e3dcc692d3457af74cd4be22640dae69c0ff
Gustavo Czobel <gus@centos.home>
2021-03-24 16:03:13 +0200

    fix: change yahav codename

--- src/definitions.ts
+++ src/definitions.ts
@@ -75,7 +75,7 @@ export const SCRAPERS = {
     loginFields: ['username', PASSWORD_FIELD],
   },
   [CompanyTypes.yahav]: {
-    name: 'Yahav',
+    name: 'Bank Yahav',
     loginFields: ['username', 'nationalID', PASSWORD_FIELD],
   },
 };
